07995aa0bf99604d2b9413ab637764ad16334fa9
FrankMugagga <frank.mugagga@gmail.com>
2023-03-02 23:32:21 +0300

    files were modified

--- src/index.js
+++ src/index.js
@@ -1,14 +1,42 @@
-import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
+const list = [
+  {
+    description: 'task1',
+    completed: true,
+    index: 1,
+  },
+  {
+    description: 'task3',
+    completed: false,
+    index: 3,
+  },
+  {
+    description: 'task2',
+    completed: true,
+    index: 2,
+  },
+];
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+list.sort((a, b) => a.index - b.index);
 
-  return element;
-}
+const listCont = document.getElementById('list');
+const populate = () => {
+  for (let i = 0; i < list.length; i += 1) {
+    const checkcompleted = list[i].completed ? 'checked' : '';
+    const li = document.createElement('li');
+    li.classList.add('list_item');
+    li.innerHTML = `
+      <div class="list_con">
+        <input type="checkbox" ${checkcompleted}>
+        <p>${list[i].description}</p>
+      </div>
+                  
+      <button class="listBtn"><i class="fa-solid fa-ellipsis-vertical"></i></button>
+    
+    `;
+    listCont.appendChild(li);
+  }
+};
 
-document.body.appendChild(component());
+populate();
