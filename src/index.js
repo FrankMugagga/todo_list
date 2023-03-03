9367d906b29c2540fc7af85642bae39775629e38
FrankMugagga <frank.mugagga@gmail.com>
2023-03-04 00:35:41 +0300

    did styling for the input and remove

--- src/index.js
+++ src/index.js
@@ -13,20 +13,21 @@ let todos = getTodos();
 function createTodoElement(todo) {
   const todoElement = document.createElement('li');
   todoElement.classList.add('list_item');
-    todoElement.innerHTML = `
+  todoElement.innerHTML = `
     <div class='list_con'>
       <input type="checkbox" ${todo.completed ? 'checked' : ''}>
       <span class='p' contenteditable="true" class="${todo.completed ? 'completed' : ''}">${todo.task}</span>    
     </div>
     <div>
-      <button class="edit-todo"><i class="fa-solid fa-ellipsis-vertical"></i></button>
-      <button class="remove-todo"><i class="fa-solid fa-trash-can"></i></button>
-      <button class="save-todo" style="display: none;">Save</button>
+      <button class="btns edit-todo"><i class="fa-solid fa-ellipsis-vertical"></i></button>
+      <button class="btns remove-todo" style="display: none;"><i class="fa-solid fa-trash-can"></i></button>
+      <button class="btns save-todo" style="display: none;">Save</button>
     </div>   
   `;
-  const editBtn = todoElement.querySelector('fa-ellipsis-vertical');
+  const editBtn = todoElement.querySelector('.fa-ellipsis-vertical');
   const todoTextElement = todoElement.querySelector('span');
   const editTodoButton = todoElement.querySelector('.edit-todo');
+  const removeTodoButton = todoElement.querySelector('.remove-todo');
   const saveTodoButton = todoElement.querySelector('.save-todo');
   /*todoTextElement.addEventListener('input', () => {
     saveTodoButton.style.display = 'inline-block';
@@ -55,10 +56,11 @@ function createTodoElement(todo) {
     todoTextElement.contentEditable = true;
     todoTextElement.focus();
     todoElement.style.background = 'lightyellow';
-    editBtn.style.display = 'none';
+    editTodoButton.style.display = 'none';
+    removeTodoButton.style.display = 'inline-block';
     //saveTodoButton.style.display = 'inline-block';
   });
-  todoElement.querySelector('button.remove-todo').addEventListener('click', () => {
+   todoElement.querySelector('button.remove-todo').addEventListener('click', () => {
     const index = todos.indexOf(todo);
     todos.splice(index, 1);
     saveTodos(todos);
