8a89a5fc4f1f89a4ce4e2a9c76f5c8a501d14609
FrankMugagga <frank.mugagga@gmail.com>
2023-03-03 18:56:08 +0300

    i added functionality for the buttons and edit

--- src/index.js
+++ src/index.js
@@ -1,42 +1,100 @@
-import './style.css';
+import { getTodos, saveTodos, clearTodos } from './modules/storage.js';
 
-const list = [
-  {
-    description: 'task1',
-    completed: true,
-    index: 1,
-  },
-  {
-    description: 'task3',
-    completed: false,
-    index: 3,
-  },
-  {
-    description: 'task2',
-    completed: true,
-    index: 2,
-  },
-];
+const todoInput = document.querySelector('#new-todo');
+const addTodoBtn = document.querySelector('#add-todo');
+const clearCompletedBtn = document.querySelector('#clear-completed');
+const clearCheckedBtn = document.querySelector('#clear-checked');
+const clearAllBtn = document.querySelector('#clear-all');
+const todosList = document.querySelector('#todos');
 
-list.sort((a, b) => a.index - b.index);
+let todos = getTodos();
 
-const listCont = document.getElementById('list');
-const populate = () => {
-  for (let i = 0; i < list.length; i += 1) {
-    const checkcompleted = list[i].completed ? 'checked' : '';
-    const li = document.createElement('li');
-    li.classList.add('list_item');
-    li.innerHTML = `
-      <div class="list_con">
-        <input type="checkbox" ${checkcompleted}>
-        <p>${list[i].description}</p>
-      </div>
-                  
-      <button class="listBtn"><i class="fa-solid fa-ellipsis-vertical"></i></button>
-    
-    `;
-    listCont.appendChild(li);
+function createTodoElement(todo) {
+  const todoElement = document.createElement('li');
+  todoElement.innerHTML = `
+    <input type="checkbox" ${todo.completed ? 'checked' : ''}>
+    <span contenteditable="true" class="${todo.completed ? 'completed' : ''}">${todo.task}</span>
+    <button class="edit-todo">Edit</button>
+    <button class="remove-todo">Remove</button>
+    <button class="save-todo" style="display: none;">Save</button>
+  `;
+  const todoTextElement = todoElement.querySelector('span');
+  const editTodoButton = todoElement.querySelector('.edit-todo');
+  const saveTodoButton = todoElement.querySelector('.save-todo');
+  todoTextElement.addEventListener('input', () => {
+    saveTodoButton.style.display = 'inline-block';
+  });
+  saveTodoButton.addEventListener('click', () => {
+    todo.task = todoTextElement.textContent.trim();
+    saveTodos(todos);
+    saveTodoButton.style.display = 'none';
+  });
+  todoElement.querySelector('input').addEventListener('change', () => {
+    todo.completed = !todo.completed;
+    saveTodos(todos);
+    if (todo.completed) {
+      todoTextElement.classList.add('completed');
+    } else {
+      todoTextElement.classList.remove('completed');
+    }
+  });
+  editTodoButton.addEventListener('click', () => {
+    todoTextElement.contentEditable = true;
+    todoTextElement.focus();
+    saveTodoButton.style.display = 'inline-block';
+  });
+  todoElement.querySelector('button.remove-todo').addEventListener('click', () => {
+    const index = todos.indexOf(todo);
+    todos.splice(index, 1);
+    saveTodos(todos);
+    todos.forEach((todo, index) => {
+      todo.index = index;
+    });
+    renderTodos();
+  });
+  return todoElement;
+}
+
+function renderTodos() {
+  todosList.innerHTML = '';
+  todos.forEach((todo, index) => {
+    todo.index = index;
+    todosList.appendChild(createTodoElement(todo));
+  });
+}
+
+addTodoBtn.addEventListener('click', () => {
+  const task = todoInput.value.trim();
+  if (task) {
+    todos.push({ task, completed: false, index: todos.length });
+    saveTodos(todos);
+    todoInput.value = '';
+    renderTodos();
   }
-};
+});
 
-populate();
+clearCompletedBtn.addEventListener('click', () => {
+  todos = todos.filter(todo => !todo.completed);
+  saveTodos(todos);
+  todos.forEach((todo, index) => {
+    todo.index = index;
+  });
+  renderTodos();
+});
+
+clearCheckedBtn.addEventListener('click', () => {
+  todos = todos.filter(todo => !todo.completed);
+  saveTodos(todos);
+  todos.forEach((todo, index) => {
+    todo.index = index;
+  });
+  renderTodos();
+});
+
+clearAllBtn.addEventListener('click', () => {
+  clearTodos();
+  todos = [];
+  renderTodos();
+});
+
+renderTodos();
