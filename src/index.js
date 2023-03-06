769f359eba7d89a2e797e6ab4877861310f822cb
FrankMugagga <frank.mugagga@gmail.com>
2023-03-06 23:40:05 +0300

    corrected linter errors

--- src/index.js
+++ src/index.js
@@ -1,150 +1,146 @@
-import { getTodos, saveTodos } from './modules/storage.js';
+import { toLocalStorage, fromLocalStorage } from './modules/storage.js';
+import './style.css';
 
-        const checkLabel = document.createElement('div');
-        checkLabel.classList.add('list_con');     
-        li.appendChild(checkLabel);
+const form = document.getElementById('list_form');
+const toAdd = document.getElementById('toAdd');
+const listCont = document.getElementById('list');
+const clearSelected = document.getElementById('clear_selected');
 
-        const checkBoxElement = document.createElement('input');
-        checkBoxElement.type = 'checkbox'
-        checkBoxElement.checked = item.completed;
-        checkBoxElement.dataset.description = item.description;
-        checkBoxElement.dataset.index = item.index;
-        checkLabel.appendChild(checkBoxElement);
-        window.addEventListener('DOMContentLoaded', () => {
+function displayList() {
+  const list = fromLocalStorage();
+  listCont.innerHTML = '';
+  list.forEach((item, listIndex) => {
+    const li = document.createElement('li');
+    li.dataset.index = item.index;
+    li.classList.add('list_item');
 
-            if(checkBoxElement.checked){
-                alert('overhere' + checkBoxElement.dataset.index);
-                list[listIndex].completed = true;
-                localStorage.setItem('list', JSON.stringify(list));
-                labelElement.style.textDecoration = 'line-through'
-                labelElement.style.color = 'red';
-            }
-            else {
-                alert('unchecked');
-                list[listIndex].completed = false;
-                localStorage.setItem('list', JSON.stringify(list));
-                labelElement.style.color = 'green';
-                labelElement.style.textDecoration = 'none'
-            }
-        });            
+    const checkLabel = document.createElement('div');
+    checkLabel.classList.add('list_con');
+    li.appendChild(checkLabel);
 
-        const labelElement = document.createElement('div');
-        labelElement.classList.add('label')
-        labelElement.textContent = item.description;
-        labelElement.dataset.index = item.index;
-        labelElement.contentEditable = false;
-        checkLabel.appendChild(labelElement);
+    const checkBoxElement = document.createElement('input');
+    checkBoxElement.type = 'checkbox';
+    checkBoxElement.checked = item.completed;
+    checkBoxElement.dataset.description = item.description;
+    checkBoxElement.dataset.index = item.index;
+    checkLabel.appendChild(checkBoxElement);
 
-        const buttonCont = document.createElement('div');
-        buttonCont.classList.add('button_cont');
-        li.appendChild(buttonCont);
+    const labelElement = document.createElement('div');
+    labelElement.classList.add('label');
+    labelElement.textContent = item.description;
+    labelElement.dataset.index = item.index;
+    labelElement.contentEditable = false;
+    checkLabel.appendChild(labelElement);
 
-        const editButton = document.createElement('button');
-        editButton.classList.add('edit_btn')
-        editButton.innerHTML = `<i class="fa-solid fa-ellipsis-vertical"></i>`;
-        editButton.dataset.index = item.index;
-        buttonCont.appendChild(editButton);
-
-        const removeButton =document.createElement('button');
-        removeButton.classList.add('remove_btn');
-        removeButton.dataset.index = item.index;
-        removeButton.innerHTML = `<i class="fa-sharp fa-solid fa-trash"></i>`;
-        buttonCont.appendChild(removeButton);
-
-        checkBoxElement.addEventListener('change', () => {
-            if(checkBoxElement.checked){
-                alert('overhere' + checkBoxElement.dataset.index);
-                list[listIndex].completed = true;
-                localStorage.setItem('list', JSON.stringify(list));
-                labelElement.style.textDecoration = 'line-through'
-                labelElement.style.color = 'red';
-            }
-            else {
-                alert('unchecked');
-                list[listIndex].completed = false;
-                localStorage.setItem('list', JSON.stringify(list));
-                labelElement.style.color = 'green';
-                labelElement.style.textDecoration = 'none'
-            }
-        });
-
-        editButton.addEventListener('click', () => {
-            
-            alert('watiiiii');
-            li.style.background = 'yellow';
-            removeButton.style.display = 'block';
-            editButton.style.display = 'none';
-
-            labelElement.contentEditable = true;
-            labelElement.addEventListener('keydown', (e) => {
-                if(e.key === 'Enter'){
-                    e.preventDefault();
-                    list[listIndex].description = labelElement.textContent;
-                    localStorage.setItem('list', JSON.stringify(list));
-                }
-
-            });
-
-            document.addEventListener('click', (e) => {
-                const isClickInside = removeButton.contains(e.target) || li.contains(e.target) || labelElement.contains(e.target);
-                if(!isClickInside) {
-                    labelElement.contentEditable = false;
-                    li.style.background = 'none';
-                    removeButton.style.display = 'none';
-                    editButton.style.display = 'block';
-                }
-
-            });
-
-            
-        });
-
-        removeButton.addEventListener('click', () => {
-            list.splice(listIndex, 1);
-            localStorage.setItem('list', JSON.stringify(list));
-
-            for (let i = 0; i < list.length; i++) {
-              list[i].index = i + 1;
-              localStorage.setItem('list', JSON.stringify(list));
-            }
-
-            listCont.removeChild(li);
-        });
-     
-
-        listCont.appendChild(li);
+    window.addEventListener('DOMContentLoaded', () => {
+      if (checkBoxElement.checked) {
+        list[listIndex].completed = true;
+        localStorage.setItem('list', JSON.stringify(list));
+        labelElement.style.textDecoration = 'line-through';
+        labelElement.style.color = 'red';
+      } else {
+        list[listIndex].completed = false;
+        localStorage.setItem('list', JSON.stringify(list));
+        labelElement.style.color = 'green';
+        labelElement.style.textDecoration = 'none';
+      }
     });
+
+    const buttonCont = document.createElement('div');
+    buttonCont.classList.add('button_cont');
+    li.appendChild(buttonCont);
+
+    const editButton = document.createElement('button');
+    editButton.classList.add('edit_btn');
+    editButton.innerHTML = '<i class="fa-solid fa-ellipsis-vertical"></i>';
+    editButton.dataset.index = item.index;
+    buttonCont.appendChild(editButton);
+
+    const removeButton = document.createElement('button');
+    removeButton.classList.add('remove_btn');
+    removeButton.dataset.index = item.index;
+    removeButton.innerHTML = '<i class="fa-sharp fa-solid fa-trash"></i>';
+    buttonCont.appendChild(removeButton);
+
+    checkBoxElement.addEventListener('change', () => {
+      if (checkBoxElement.checked) {
+        list[listIndex].completed = true;
+        localStorage.setItem('list', JSON.stringify(list));
+        labelElement.style.textDecoration = 'line-through';
+        labelElement.style.color = 'red';
+      } else {
+        list[listIndex].completed = false;
+        localStorage.setItem('list', JSON.stringify(list));
+        labelElement.style.color = 'green';
+        labelElement.style.textDecoration = 'none';
+      }
+    });
+
+    editButton.addEventListener('click', () => {
+      li.style.background = 'yellow';
+      removeButton.style.display = 'block';
+      editButton.style.display = 'none';
+
+      labelElement.contentEditable = true;
+      labelElement.addEventListener('keydown', (e) => {
+        if (e.key === 'Enter') {
+          e.preventDefault();
+          list[listIndex].description = labelElement.textContent;
+          localStorage.setItem('list', JSON.stringify(list));
+        }
+      });
+
+      document.addEventListener('click', (e) => {
+        const isClickInside = removeButton.contains(e.target)
+        || li.contains(e.target) || labelElement.contains(e.target);
+        if (!isClickInside) {
+          labelElement.contentEditable = false;
+          li.style.background = 'none';
+          removeButton.style.display = 'none';
+          editButton.style.display = 'block';
+        }
+      });
+    });
+
+    removeButton.addEventListener('click', () => {
+      list.splice(listIndex, 1);
+      localStorage.setItem('list', JSON.stringify(list));
+
+      for (let i = 0; i < list.length; i = +1) {
+        list[i].index = i + 1;
+        localStorage.setItem('list', JSON.stringify(list));
+      }
+
+      listCont.removeChild(li);
+    });
+
+    listCont.appendChild(li);
+  });
 }
 
 form.addEventListener('submit', (e) => {
-   e.preventDefault();
+  e.preventDefault();
 
-   if(toAdd.value) {
-    const list = toLocalStorage(toAdd.value);
-    alert(list.length);
-    displayList();        
-   }
-   else {
-    alert('an input is required');
-   }
-  
-   form.reset();
+  if (toAdd.value) {
+    toLocalStorage(toAdd.value);
+    displayList();
+  }
+
+  form.reset();
 });
 
 clearSelected.addEventListener('click', () => {
-    let list = JSON.parse(localStorage.getItem('list')) || [];
-    list = list.filter(item => !item.completed);
+  let list = JSON.parse(localStorage.getItem('list')) || [];
+  list = list.filter((item) => !item.completed);
+  localStorage.setItem('list', JSON.stringify(list));
+
+  for (let i = 0; i < list.length; i = +1) {
+    const list = JSON.parse(localStorage.getItem('list')) || [];
+    list[i].index = i + 1;
     localStorage.setItem('list', JSON.stringify(list));
+  }
 
-    for (let i = 0; i < list.length; i++) {
-        let list = JSON.parse(localStorage.getItem('list')) || [];
-      list[i].index = i + 1;
-      localStorage.setItem('list', JSON.stringify(list));
-    }
-   
-    displayList(); 
-  });
-
+  displayList();
+});
 
 displayList();
-
--- src/modules/storage.js
+++ src/modules/storage.js
@@ -1,14 +1,14 @@
 export function toLocalStorage(toAdd) {
   const list = JSON.parse(localStorage.getItem('list')) || [];
   const listObject = {
-      description: toAdd,
-      completed: false,
-      index: list.length + 1,
+    description: toAdd,
+    completed: false,
+    index: list.length + 1,
   };
 
   list.push(listObject);
   localStorage.setItem('list', JSON.stringify(list));
-  localStorage.removeItem("todoList");
+  localStorage.removeItem('todoList');
 
   return list;
 }
@@ -16,13 +16,9 @@ export function toLocalStorage(toAdd) {
 export function fromLocalStorage() {
   const list = JSON.parse(localStorage.getItem('list')) || [];
 
-  return list.map((list, listIndex) => {
-      return {
-          description: list.description,
-          completed: list.completed,
-          index: listIndex + 1,
-      };
-  });
+  return list.map((list, listIndex) => ({
+    description: list.description,
+    completed: list.completed,
+    index: listIndex + 1,
+  }));
 }
-
-
