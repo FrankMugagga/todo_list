a155247ffa2904aba537dabdba034587c9916ab4
FrankMugagga <frank.mugagga@gmail.com>
2023-03-06 22:43:03 +0300

    did button functionality, display and linkages to modules

--- src/index.js
+++ src/index.js
@@ -1,95 +1,163 @@
-import { getTodos, saveTodos } from './modules/storage.js';
-import './style.css';
+import { toLocalStorage, fromLocalStorage } from './modules/storage.js';
 
-const todoInput = document.querySelector('#new-todo');
-const addTodoBtn = document.querySelector('#add-todo');
-const clearCheckedBtn = document.querySelector('#clear-checked');
-const todosList = document.querySelector('#todos');
+const form = document.getElementById('list_form');
+const toAdd = document.getElementById('toAdd');
+const listCont = document.getElementById('list');
+const clearSelected = document.getElementById('clear_selected');
 
-let todos = getTodos();
+function displayList() {
+    const list = fromLocalStorage();
+    listCont.innerHTML = '';
+    list.forEach((item, listIndex) => {
+        const li = document.createElement('li');
+        li.dataset.index = item.index;
+        li.classList.add('list_item');
 
-function createTodoElement(todo) {
-  const todoElement = document.createElement('li');
-  todoElement.classList.add('list_item');
-  todoElement.innerHTML = `
-    <div class='list_con'>
-      <input class="input1" type="checkbox" ${todo.completed ? 'checked' : ''}>
-      <span class='p' contenteditable="true" class="${todo.completed ? 'completed' : ''}">${todo.task}</span>    
-    </div>
-    <div>
-      <button class="btns edit-todo"><i class="fa-solid fa-ellipsis-vertical"></i></button>
-      <button class="btns remove-todo" style="display: none;"><i class="fa-solid fa-trash-can"></i></button>
-      <button class="btns save-todo" style="display: none;">Save</button>
-    </div>   
-  `;
-  const todoTextElement = todoElement.querySelector('span');
-  const editTodoButton = todoElement.querySelector('.edit-todo');
-  const removeTodoButton = todoElement.querySelector('.remove-todo');
-  const saveTodoButton = todoElement.querySelector('.save-todo');
-  todoTextElement.addEventListener('keydown', (event) => {
-    if (event.key === 'Enter') {
-      event.preventDefault();
-      saveTodoButton.click();
-    }
-  });
-  saveTodoButton.addEventListener('click', () => {
-    todo.task = todoTextElement.textContent.trim();
-    saveTodos(todos);
-    saveTodoButton.style.display = 'none';
-  });
-  todoElement.querySelector('input').addEventListener('change', () => {
-    todo.completed = !todo.completed;
-    saveTodos(todos);
-    if (todo.completed) {
-      todoTextElement.classList.add('completed');
-    } else {
-      todoTextElement.classList.remove('completed');
-    }
-  });
-  editTodoButton.addEventListener('click', () => {
-    todoTextElement.contentEditable = true;
-    todoElement.style.background = 'lightyellow';
-    editTodoButton.style.display = 'none';
-    removeTodoButton.style.display = 'inline-block';
-  });
-  todoElement.querySelector('button.remove-todo').addEventListener('click', () => {
-    const index = todos.indexOf(todo);
-    todos.splice(index, 1);
-    saveTodos(todos);
-    todos.forEach((todo, index) => {
-      todo.index = index;
+        const checkLabel = document.createElement('div');
+        checkLabel.classList.add('list_con');     
+        li.appendChild(checkLabel);
+
+        const checkBoxElement = document.createElement('input');
+        checkBoxElement.type = 'checkbox'
+        checkBoxElement.checked = item.completed;
+        checkBoxElement.dataset.description = item.description;
+        checkBoxElement.dataset.index = item.index;
+        checkLabel.appendChild(checkBoxElement);
+        window.addEventListener('DOMContentLoaded', () => {
+
+            if(checkBoxElement.checked){
+                alert('overhere' + checkBoxElement.dataset.index);
+                list[listIndex].completed = true;
+                localStorage.setItem('list', JSON.stringify(list));
+                labelElement.style.textDecoration = 'line-through'
+                labelElement.style.color = 'red';
+            }
+            else {
+                alert('unchecked');
+                list[listIndex].completed = false;
+                localStorage.setItem('list', JSON.stringify(list));
+                labelElement.style.color = 'green';
+                labelElement.style.textDecoration = 'none'
+            }
+        });            
+
+        const labelElement = document.createElement('div');
+        labelElement.classList.add('label')
+        labelElement.textContent = item.description;
+        labelElement.dataset.index = item.index;
+        labelElement.contentEditable = false;
+        checkLabel.appendChild(labelElement);
+
+        const buttonCont = document.createElement('div');
+        buttonCont.classList.add('button_cont');
+        li.appendChild(buttonCont);
+
+        const editButton = document.createElement('button');
+        editButton.classList.add('edit_btn')
+        editButton.innerHTML = `<i class="fa-solid fa-ellipsis-vertical"></i>`;
+        editButton.dataset.index = item.index;
+        buttonCont.appendChild(editButton);
+
+        const removeButton =document.createElement('button');
+        removeButton.classList.add('remove_btn');
+        removeButton.dataset.index = item.index;
+        removeButton.innerHTML = `<i class="fa-sharp fa-solid fa-trash"></i>`;
+        buttonCont.appendChild(removeButton);
+
+        checkBoxElement.addEventListener('change', () => {
+            if(checkBoxElement.checked){
+                alert('overhere' + checkBoxElement.dataset.index);
+                list[listIndex].completed = true;
+                localStorage.setItem('list', JSON.stringify(list));
+                labelElement.style.textDecoration = 'line-through'
+                labelElement.style.color = 'red';
+            }
+            else {
+                alert('unchecked');
+                list[listIndex].completed = false;
+                localStorage.setItem('list', JSON.stringify(list));
+                labelElement.style.color = 'green';
+                labelElement.style.textDecoration = 'none'
+            }
+        });
+
+        editButton.addEventListener('click', () => {
+            
+            alert('watiiiii');
+            li.style.background = 'yellow';
+            removeButton.style.display = 'block';
+            editButton.style.display = 'none';
+
+            labelElement.contentEditable = true;
+            labelElement.addEventListener('keydown', (e) => {
+                if(e.key === 'Enter'){
+                    e.preventDefault();
+                    list[listIndex].description = labelElement.textContent;
+                    localStorage.setItem('list', JSON.stringify(list));
+                }
+
+            });
+
+            document.addEventListener('click', (e) => {
+                const isClickInside = removeButton.contains(e.target) || li.contains(e.target) || labelElement.contains(e.target);
+                if(!isClickInside) {
+                    labelElement.contentEditable = false;
+                    li.style.background = 'none';
+                    removeButton.style.display = 'none';
+                    editButton.style.display = 'block';
+                }
+
+            });
+
+            
+        });
+
+        removeButton.addEventListener('click', () => {
+            list.splice(listIndex, 1);
+            localStorage.setItem('list', JSON.stringify(list));
+
+            for (let i = 0; i < list.length; i++) {
+              list[i].index = i + 1;
+              localStorage.setItem('list', JSON.stringify(list));
+            }
+
+            listCont.removeChild(li);
+        });
+     
+
+        listCont.appendChild(li);
     });
-    todosList.innerHTML = '';
-    todos.forEach((todo, index) => {
-      todo.index = index;
-      todosList.appendChild(createTodoElement(todo));
-    });
-  });
-  return todoElement;
 }
-function renderTodos() {
-  todosList.innerHTML = '';
-  todos.forEach((todo, index) => {
-    todo.index = index;
-    todosList.appendChild(createTodoElement(todo));
-  });
-}
-addTodoBtn.addEventListener('click', () => {
-  const task = todoInput.value.trim();
-  if (task) {
-    todos.push({ task, completed: false, index: todos.length });
-    saveTodos(todos);
-    todoInput.value = '';
-    renderTodos();
-  }
-});
-clearCheckedBtn.addEventListener('click', () => {
-  todos = todos.filter((todo) => !todo.completed);
-  saveTodos(todos);
-  todos.forEach((todo, index) => {
-    todo.index = index;
-  });
-  renderTodos();
+
+form.addEventListener('submit', (e) => {
+   e.preventDefault();
+
+   if(toAdd.value) {
+    const list = toLocalStorage(toAdd.value);
+    alert(list.length);
+    displayList();        
+   }
+   else {
+    alert('an input is required');
+   }
+  
+   form.reset();
 });
 
-renderTodos();
+clearSelected.addEventListener('click', () => {
+    let list = JSON.parse(localStorage.getItem('list')) || [];
+    list = list.filter(item => !item.completed);
+    localStorage.setItem('list', JSON.stringify(list));
+
+    for (let i = 0; i < list.length; i++) {
+        let list = JSON.parse(localStorage.getItem('list')) || [];
+      list[i].index = i + 1;
+      localStorage.setItem('list', JSON.stringify(list));
+    }
+   
+    displayList(); 
+  });
+
+
+displayList();
+
