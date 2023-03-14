cf6a51f4dd8061197f647314165efc0298e4d5a1
FrankMugagga <frank.mugagga@gmail.com>
2023-03-14 12:58:42 +0300

    Add const toAdd and also copied form element from index.js

--- src/modules/eventListeners.js
+++ src/modules/eventListeners.js
@@ -1,27 +1,30 @@
 import displayList from './display.js';
 import { toLocalStorage } from './storage.js';
 
+const toAdd = document.getElementById('toAdd');
+const form = document.getElementById('list_form');
+
 export const inputValue = (e) => {
-    e.preventDefault();
-  
-    if (toAdd.value) {
-      toLocalStorage(toAdd.value);
-      displayList();
-    }
-  
-    form.reset();
-  };
-  
-  export  const clearSelectOnly = () => {
-    let list = JSON.parse(localStorage.getItem('list')) || [];
-    list = list.filter((item) => !item.completed);
-    localStorage.setItem('list', JSON.stringify(list));
-  
-    for (let i = 0; i < list.length; i += 1) {
-      const list = JSON.parse(localStorage.getItem('list')) || [];
-      list[i].index = i + 1;
-      localStorage.setItem('list', JSON.stringify(list));
-    }
-  
+  e.preventDefault();
+
+  if (toAdd.value) {
+    toLocalStorage(toAdd.value);
     displayList();
-  }; 
+  }
+
+  form.reset();
+};
+
+export const clearSelectOnly = () => {
+  let list = JSON.parse(localStorage.getItem('list')) || [];
+  list = list.filter((item) => !item.completed);
+  localStorage.setItem('list', JSON.stringify(list));
+
+  for (let i = 0; i < list.length; i += 1) {
+    const list = JSON.parse(localStorage.getItem('list')) || [];
+    list[i].index = i + 1;
+    localStorage.setItem('list', JSON.stringify(list));
+  }
+
+  displayList();
+};
