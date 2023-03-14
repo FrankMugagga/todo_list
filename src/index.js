1317bfde5fa60d39cbcaf8f3159a0be7d3fe7ac9
FrankMugagga <frank.mugagga@gmail.com>
2023-03-14 12:57:33 +0300

    Remove const toAdd and also copied form element to eventListeners.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import displayList from './modules/display.js';
-import { inputValue, clearSelectOnly  } from './modules/eventListeners.js';
+import { inputValue, clearSelectOnly } from './modules/eventListeners.js';
 import './style.css';
 
 const form = document.getElementById('list_form');
-const toAdd = document.getElementById('toAdd');
 const clearSelected = document.getElementById('clear_selected');
 
 form.addEventListener('submit', inputValue);
