6a8590cc8355cafdb1cb7ae023f0ec26febb395c
FrankMugagga <frank.mugagga@gmail.com>
2023-03-14 12:47:03 +0300

    Made modules for event listener functions: clearSelectOnly and inputValue

--- src/index.js
+++ src/index.js
@@ -1,34 +1,12 @@
-import { toLocalStorage } from './modules/storage.js';
 import displayList from './modules/display.js';
-//import './style.css';
+import { inputValue, clearSelectOnly  } from './modules/eventListeners.js';
+import './style.css';
 
 const form = document.getElementById('list_form');
 const toAdd = document.getElementById('toAdd');
 const clearSelected = document.getElementById('clear_selected');
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-
-  if (toAdd.value) {
-    toLocalStorage(toAdd.value);
-    displayList();
-  }
-
-  form.reset();
-});
-
-clearSelected.addEventListener('click', () => {
-  let list = JSON.parse(localStorage.getItem('list')) || [];
-  list = list.filter((item) => !item.completed);
-  localStorage.setItem('list', JSON.stringify(list));
-
-  for (let i = 0; i < list.length; i += 1) {
-    const list = JSON.parse(localStorage.getItem('list')) || [];
-    list[i].index = i + 1;
-    localStorage.setItem('list', JSON.stringify(list));
-  }
-
-  displayList();
-});
+form.addEventListener('submit', inputValue);
+clearSelected.addEventListener('click', clearSelectOnly);
 
 displayList();
